cefcb27b8673e8643e1fdfc8b00e41445f05a69f
Pepelirrojo <josete_8@hotmail.com>
2021-08-06 12:04:34 +0200

    Diccionario terminado en español

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -3,6 +3,7 @@ package main;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -11,15 +12,24 @@ public class Principal {
 		ApiRequest myApiRequest = new ApiRequest();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduzca la palabra que desea buscar:");
-		String word = sc.next();
-		String path = "https://api.dictionaryapi.dev/api/v2/entries/es/" + word;
-		try {
-			String response = myApiRequest.getRequest(path);
-			JSONObject respuesta = (JSONObject) JSONValue.parse(response.toString());
-			System.out.println(respuesta.toJSONString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		while (sc.hasNext()) {
+			String word = sc.next();
+			String path = "https://api.dictionaryapi.dev/api/v2/entries/es/" + word;
+			try {
+				String response = myApiRequest.getRequest(path);
+				JSONArray respuesta = (JSONArray) JSONValue.parse(response.toString());
+				JSONObject row = (JSONObject) respuesta.get(0);
+				JSONArray meanings = (JSONArray) row.get("meanings");
+				JSONObject rowDefinitions = (JSONObject) meanings.get(0);
+				JSONArray definitions = (JSONArray) rowDefinitions.get("definitions");
+				JSONObject rowDefinitions2 = (JSONObject) definitions.get(0);
+				String definition = rowDefinitions2.get("definition").toString();
+				System.out.println("Definicion:" + definition);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 	}
